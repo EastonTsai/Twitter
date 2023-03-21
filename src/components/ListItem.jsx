4d1bcc478cb8f57a432c55148c79bf6cd2b90b6e
EastonTsai <eastontsai.te@gmail.com>
2023-03-21 16:34:12 +0800

    followers 未完待續

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,47 @@
+
+
+import styles from 'styles/components/listItem.module.css'
+import { ReactComponent as UserIcon } from 'files/icon/logo.svg';
+import { Btn, FollowTab } from 'components/Common';
+// import { ReactComponent as IsFollowIcon } from 'files/icon/'
+
+//這是 '一篇' 文章的樣式
+export const ListItem = ({
+  avatar,
+  name,
+  account,
+  text,
+  isFollow,
+}) => {
+
+  /*按 XX do something... */
+  const handleClick = () => { }
+
+  return(
+    <li className={styles.wrap}>
+      <div className={styles.userAvatar}> 
+        {/*預設沒傳頭像的顯示 */}{/*我在 vscode 上看 <img> 下有波浪文 , 不知什麼問題 ? */}
+        {avatar ? <img src={avatar} /> : <UserIcon/>}
+      </div>
+      <div className={styles.tweetContent}>
+        <div className={styles.topWrap}>
+          <h3 className={styles.name}>
+            {name} 
+            <span className={styles.account}> {account} </span> 
+          </h3>
+          <div 
+            className={styles.button}
+            onClick={handleClick}
+          >
+            {isFollow ?
+            <Btn className={'btnRoundColor'} text={'正在跟隨'} /> :
+            <Btn className={'btnRound'} text={'跟隨'} /> 
+          }
+          </div>
+        </div>
+        <p className={styles.text}> {text} </p>
+      </div>
+    </li>
+  )
+} 
+export default ListItem ;
---
+++ src/demmy/demmy.js
@@ -0,0 +1,282 @@
+    
+    
+    
+    
+    
+    //是一個陣列 , 裡面有很多物件 , 物件裡才是各種資料
+    export let demmy ={data:[
+      {
+        "role": "user or admin", // user || admin,
+        "id": 5,
+        "account":"user5",
+        "password": "12345",
+        "email":"user5@example.com",
+        "name": "user5 name",							
+        "avator":"https://picsum.photos/500/400?random=2",
+        "coverPage":"https://picsum.photos/500/400?random=1",
+        "description":"推文內容, 推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,",
+        "createdAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "updatedAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "followingCounts": 12,
+        "followerCounts": 10,
+        "likeCounts": 10,
+        "replyCounts": 30,
+        "introduction":"自我介紹", //自我介紹
+        "checkPassword": "12345",
+        "isFollowed": true, // 是否也被當前使用者追蹤
+        "isCurrentUser": false,// 是否包含當前登入使用者 ; 目前登入者是否在前10名當中
+        "userId": 10, // 當前使用者想要追蹤對象的 user id
+      },
+      {
+        "role": "user or admin", // user || admin,
+        "id": 5,
+        "account":"user5",
+        "password": "12345",
+        "email":"user5@example.com",
+        "name": "user5 name",							
+        "avator":"https://picsum.photos/500/400?random=2",
+        "coverPage":"https://picsum.photos/500/400?random=1",
+        "description":"推文內容, 推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,",
+        "createdAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "updatedAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "followingCounts": 12,
+        "followerCounts": 10,
+        "likeCounts": 10,
+        "replyCounts": 30,
+        "introduction":"自我介紹", //自我介紹
+        "checkPassword": "12345",
+        "isFollowed": true, // 是否也被當前使用者追蹤
+        "isCurrentUser": false,// 是否包含當前登入使用者 ; 目前登入者是否在前10名當中
+        "userId": 10, // 當前使用者想要追蹤對象的 user id
+      },
+      {
+        "role": "user or admin", // user || admin,
+        "id": 5,
+        "account":"user5",
+        "password": "12345",
+        "email":"user5@example.com",
+        "name": "user5 name",							
+        "avator":"https://picsum.photos/500/400?random=2",
+        "coverPage":"https://picsum.photos/500/400?random=1",
+        "description":"推文內容, 推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,",
+        "createdAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "updatedAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "followingCounts": 12,
+        "followerCounts": 10,
+        "likeCounts": 10,
+        "replyCounts": 30,
+        "introduction":"自我介紹", //自我介紹
+        "checkPassword": "12345",
+        "isFollowed": true, // 是否也被當前使用者追蹤
+        "isCurrentUser": false,// 是否包含當前登入使用者 ; 目前登入者是否在前10名當中
+        "userId": 10, // 當前使用者想要追蹤對象的 user id
+      },
+      {
+        "role": "user or admin", // user || admin,
+        "id": 5,
+        "account":"user5",
+        "password": "12345",
+        "email":"user5@example.com",
+        "name": "user5 name",							
+        "avator":"https://picsum.photos/500/400?random=2",
+        "coverPage":"https://picsum.photos/500/400?random=1",
+        "description":"推文內容, 推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,",
+        "createdAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "updatedAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "followingCounts": 12,
+        "followerCounts": 10,
+        "likeCounts": 10,
+        "replyCounts": 30,
+        "introduction":"自我介紹", //自我介紹
+        "checkPassword": "12345",
+        "isFollowed": true, // 是否也被當前使用者追蹤
+        "isCurrentUser": false,// 是否包含當前登入使用者 ; 目前登入者是否在前10名當中
+        "userId": 10, // 當前使用者想要追蹤對象的 user id
+      },
+      {
+        "role": "user or admin", // user || admin,
+        "id": 5,
+        "account":"user5",
+        "password": "12345",
+        "email":"user5@example.com",
+        "name": "user5 name",							
+        "avator":"https://picsum.photos/500/400?random=2",
+        "coverPage":"https://picsum.photos/500/400?random=1",
+        "description":"推文內容, 推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,",
+        "createdAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "updatedAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "followingCounts": 12,
+        "followerCounts": 10,
+        "likeCounts": 10,
+        "replyCounts": 30,
+        "introduction":"自我介紹", //自我介紹
+        "checkPassword": "12345",
+        "isFollowed": true, // 是否也被當前使用者追蹤
+        "isCurrentUser": false,// 是否包含當前登入使用者 ; 目前登入者是否在前10名當中
+        "userId": 10, // 當前使用者想要追蹤對象的 user id
+      },
+      {
+        "role": "user or admin", // user || admin,
+        "id": 5,
+        "account":"user5",
+        "password": "12345",
+        "email":"user5@example.com",
+        "name": "user5 name",							
+        "avator":"https://picsum.photos/500/400?random=2",
+        "coverPage":"https://picsum.photos/500/400?random=1",
+        "description":"推文內容, 推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,",
+        "createdAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "updatedAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "followingCounts": 12,
+        "followerCounts": 10,
+        "likeCounts": 10,
+        "replyCounts": 30,
+        "introduction":"自我介紹", //自我介紹
+        "checkPassword": "12345",
+        "isFollowed": true, // 是否也被當前使用者追蹤
+        "isCurrentUser": false,// 是否包含當前登入使用者 ; 目前登入者是否在前10名當中
+        "userId": 10, // 當前使用者想要追蹤對象的 user id
+      },
+      {
+        "role": "user or admin", // user || admin,
+        "id": 5,
+        "account":"user5",
+        "password": "12345",
+        "email":"user5@example.com",
+        "name": "user5 name",							
+        "avator":"https://picsum.photos/500/400?random=2",
+        "coverPage":"https://picsum.photos/500/400?random=1",
+        "description":"推文內容, 推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,",
+        "createdAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "updatedAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "followingCounts": 12,
+        "followerCounts": 10,
+        "likeCounts": 10,
+        "replyCounts": 30,
+        "introduction":"自我介紹", //自我介紹
+        "checkPassword": "12345",
+        "isFollowed": true, // 是否也被當前使用者追蹤
+        "isCurrentUser": false,// 是否包含當前登入使用者 ; 目前登入者是否在前10名當中
+        "userId": 10, // 當前使用者想要追蹤對象的 user id
+      },
+      {
+        "role": "user or admin", // user || admin,
+        "id": 5,
+        "account":"user5",
+        "password": "12345",
+        "email":"user5@example.com",
+        "name": "user5 name",							
+        "avator":"https://picsum.photos/500/400?random=2",
+        "coverPage":"https://picsum.photos/500/400?random=1",
+        "description":"推文內容, 推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,",
+        "createdAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "updatedAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "followingCounts": 12,
+        "followerCounts": 10,
+        "likeCounts": 10,
+        "replyCounts": 30,
+        "introduction":"自我介紹", //自我介紹
+        "checkPassword": "12345",
+        "isFollowed": true, // 是否也被當前使用者追蹤
+        "isCurrentUser": false,// 是否包含當前登入使用者 ; 目前登入者是否在前10名當中
+        "userId": 10, // 當前使用者想要追蹤對象的 user id
+      },
+      {
+        "role": "user or admin", // user || admin,
+        "id": 5,
+        "account":"user5",
+        "password": "12345",
+        "email":"user5@example.com",
+        "name": "user5 name",							
+        "avator":"https://picsum.photos/500/400?random=2",
+        "coverPage":"https://picsum.photos/500/400?random=1",
+        "description":"推文內容, 推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,",
+        "createdAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "updatedAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "followingCounts": 12,
+        "followerCounts": 10,
+        "likeCounts": 10,
+        "replyCounts": 30,
+        "introduction":"自我介紹", //自我介紹
+        "checkPassword": "12345",
+        "isFollowed": true, // 是否也被當前使用者追蹤
+        "isCurrentUser": false,// 是否包含當前登入使用者 ; 目前登入者是否在前10名當中
+        "userId": 10, // 當前使用者想要追蹤對象的 user id
+      },
+      {
+        "role": "user or admin", // user || admin,
+        "id": 5,
+        "account":"user5",
+        "password": "12345",
+        "email":"user5@example.com",
+        "name": "user5 name",							
+        "avator":"https://picsum.photos/500/400?random=2",
+        "coverPage":"https://picsum.photos/500/400?random=1",
+        "description":"推文內容, 推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,",
+        "createdAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "updatedAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "followingCounts": 12,
+        "followerCounts": 10,
+        "likeCounts": 10,
+        "replyCounts": 30,
+        "introduction":"自我介紹", //自我介紹
+        "checkPassword": "12345",
+        "isFollowed": true, // 是否也被當前使用者追蹤
+        "isCurrentUser": false,// 是否包含當前登入使用者 ; 目前登入者是否在前10名當中
+        "userId": 10, // 當前使用者想要追蹤對象的 user id
+      },
+      {
+        "role": "user or admin", // user || admin,
+        "id": 5,
+        "account":"user5",
+        "password": "12345",
+        "email":"user5@example.com",
+        "name": "user5 name",							
+        "avator":"https://picsum.photos/500/400?random=2",
+        "coverPage":"https://picsum.photos/500/400?random=1",
+        "description":"推文內容, 推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,",
+        "createdAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "updatedAt": "YYYY-MM-DDThh:mm:ss.000Z",
+        "followingCounts": 12,
+        "followerCounts": 10,
+        "likeCounts": 10,
+        "replyCounts": 30,
+        "introduction":"自我介紹", //自我介紹
+        "checkPassword": "12345",
+        "isFollowed": true, // 是否也被當前使用者追蹤
+        "isCurrentUser": false,// 是否包含當前登入使用者 ; 目前登入者是否在前10名當中
+        "userId": 10, // 當前使用者想要追蹤對象的 user id
+      },
+    ]}
+//全站所有資料 , 層級較多
+    export let allDemmy = {
+      'status': 'success',
+      'message': '各種錯誤訊息',
+      'data': [ //data 是 -陣列- , 裡面有很多份 -物件- , 物件裡很多 -資料-
+        {
+        'token': 'token',
+        'user':{
+          "role": "user or admin", // user || admin,
+          "id": 5,
+          "account":"user5",
+          "password": "12345",
+          "email":"user5@example.com",
+          "name": "user5 name",							
+          "avator":"https://picsum.photos/500/400?random=2",
+          "coverPage":"https://picsum.photos/500/400?random=1",
+          "description":"推文內容, 推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,推文內容,",
+          "createdAt": "YYYY-MM-DDThh:mm:ss.000Z",
+          "updatedAt": "YYYY-MM-DDThh:mm:ss.000Z",
+          "followingCounts": 12,
+          "followerCounts": 10,
+          "likeCounts": 10,
+          "replyCounts": 30,
+          "introduction":"自我介紹", //自我介紹
+          "checkPassword": "12345",
+          "isFollowed": true, // 是否也被當前使用者追蹤
+          "isCurrentUser": false,// 是否包含當前登入使用者 ; 目前登入者是否在前10名當中
+          "userId": 10, // 當前使用者想要追蹤對象的 user id
+        },
+      }
+    ]
+    }
