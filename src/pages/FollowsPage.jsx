56b50ebad54646e47f3506665e31409a0b3691b6
EastonTsai <eastontsai.te@gmail.com>
2023-03-25 14:27:12 +0800

    進入首頁立即取得全站推文, postbox 裡可新增推文, followsPage 的頁面資料跳轉

--- src/pages/FollowsPage.jsx
+++ src/pages/FollowsPage.jsx
@@ -1,12 +1,12 @@
-import { dummy, dummy2 } from "dummy/dummy"
 import styles from "styles/pages/profilePage.module.css"
 import { RecommendBoard } from "components/RecommendBoard"
 import { ReactComponent as Back } from "files/icon/back.svg"
 import { Link, useLocation } from "react-router-dom"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { FollowTab } from "components/Common"
 import FollowListItem from "components/FollowListItem"
-
+import { getUserFollowersApi, getUserFollowingsApi } from "api/CRUD"
+const user = {name: 'Joe 寫史的資料' }//考處之後在全站記錄的使用者個資
 
 export default function FollowsPage() {
   const location = useLocation()
@@ -17,51 +17,80 @@ export default function FollowsPage() {
   const [tabIndex, setTabIndex] = useState(Number(index)) 
   //記錄當前列要顯示 '追隨者' 或 '正在追隨' 清單
   const [currenPage, setCurrenPage] = useState(null) 
-
-  //----------------------
-  //進到畫面很 , 如何顯示和 index 相對應的清單 ..待解決
-  //----------------------
-
-  const res = dummy.data //串假資料
-  function handleClick(event) {
-    const tabName = event.target.textContent
-    if (tabName === "追隨者") {
-      setTabIndex(0)
-      setCurrenPage(() => { return getItems(dummy.data)})
-    } else if (tabName === "正在追隨") {
-      setTabIndex(1)
-      setCurrenPage(() => { return getItems(dummy2.data)})
+  
+  //進到畫面後 , 依照 tabIndex 渲染相對的资料
+  useEffect(()=>{
+    const infoItPage = ()=>{
+      const id = localStorage.getItem('id')
+      if(tabIndex === 0){ //取追隨者
+        getFollowers(id)
+        return
+      }
+      getFollowings(id)
     }
+    infoItPage()
+  },[])
+  //在畫面中 , 點擊 Tab 會渲染出相對清單的流程-------
+  //若沒有回傳的資料也會渲染空白頁
+  function handleClickTab(event) {
+    const tabName = event.target.textContent
+    const id = localStorage.getItem('id')
+    if (tabName === "追隨者") {
+      getFollowers(id)
+      return
+    }
+    getFollowings(id)
   }
-  //取得 items 參數是 資料 = 陣列 
+  //怎麼取資料的方法----------
+  const getFollowers = async (id)=>{
+      const data = await getUserFollowersApi(id)
+      setTabIndex(0)
+      if(!data || data.length < 1){
+        //給 null 是因為如果點了另一個是空也要渲染, 不是會留下原本的 
+        setCurrenPage(null)
+        console.log('沒有任何 " 跟隨者 "')
+      }
+      setCurrenPage(() => { return getItems(data)})
+
+  }
+  const getFollowings = async (id)=>{
+      const data = await getUserFollowingsApi(id)
+      setTabIndex(1)
+      if(!data || data.length < 1){
+        setCurrenPage(null)
+        console.log('沒有 " 跟隨 " 任何人')
+      }
+      setCurrenPage(() => { return getItems(data)})
+    }
+  //取得資料後 , 渲染出清單的流程 ------------------- 
   function getItems(data) {
-    let listItems = data.map( dm => {
+    let listItems = data.map( user => {
       return(
         <FollowListItem
-          key={dm.id}
-          name={dm.name}
-          text={dm.description}
-          isFollow= {dm.isFollowed}
-          avatar={dm.avator}
+          key={user.followerId}
+          name={user.name}
+          avatar={user.avatar}
+          text={user.introduction}
+          isFollow= {user.isFollowed}
         />
       )
     })
     return listItems;
-  }
+  }//--------------------------------------------
   return(
     <>
       <main className={`col-6 ${styles.mainStyle}`}>
         <div className={styles.linkContainer}>
-          <Link to="/tweets" className={styles.backLink}>
+          <Link to="/ProfilePage" className={styles.backLink}>
             <Back />
           </Link>
           <div className={styles.textBox}>
-            <h5>{res.name}</h5>
+            <h5>{user.name}</h5>
             <div className={styles.tweet}>25推文</div>
           </div>
         </div>
         <FollowTab 
-          onClick={handleClick} 
+          onClick={handleClickTab} 
           currentTab={tabIndex} 
         />
         <div className={`container-fulid ${styles.listContainer}`}>
